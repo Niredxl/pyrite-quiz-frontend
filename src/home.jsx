5534541761c84d44a143718e1b923a1a66c6b53a
Jobeal M Joseph <jobeal2020@gmail.com>
2025-12-19 09:30:37 +0530

    Changed name

--- src/home.jsx
+++ src/home.jsx
@@ -74,7 +74,7 @@ function Home({ user, onStartQuiz, onLogout, items, loading, error }) {
       {/* Main Content */}
 
       <div className="home-container">
-        <h1 className="home-title">MP & MC</h1>
+        <h1 className="home-title">AAD</h1>
         {/* ✅ 3. Conditionally render based on whether the quiz has been attempted */}
         {quizAttempted ? (
           <div className="result-display">
